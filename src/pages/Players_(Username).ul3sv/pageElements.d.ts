4e654a54c4b80ee0a4d50bc661c2025eb52814b5
etai <etai@wix.com>
2020-04-29 23:40:10 +0300

    add games and players pages

---
+++ src/pages/Players_(Username).ul3sv/pageElements.d.ts
@@ -0,0 +1,21 @@
+type PageElementsMap = {
+  "#page1": $w.Page;
+  "#ListStrip": $w.ColumnStrip;
+  "#dynamicDataset": $w.router_dataset;
+  "#dataset1": $w.dataset;
+  "#column1": $w.Column;
+  "#title": $w.Text;
+  "#text17": $w.Text;
+  "#table1": $w.Table;
+  "#group1": $w.Box;
+  "#name": $w.Text;
+  "#text16": $w.Text;
+  "#header1": $w.Header;
+  "#footer1": $w.Footer;
+  "#container1": $w.HiddenCollapsedElement;
+  "#text15": $w.Text;
+  "#accountNavBar1": $w.AccountNavBar;
+  "#horizontalMenu1": $w.Menu;
+  "#profile1": $w.IFrame;
+  "#verticalMenu1": $w.Menu;
+}
